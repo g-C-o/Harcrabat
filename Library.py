a8bb12bae8ac0fdc2f3f0394c621f975e8753bc4
Cameron White <cameronw@davidsonacademy.unr.edu>
2019-12-16 11:07:26 -0800

    Reorganize Database and rename to Library

--- Library.py
+++ Library.py
@@ -1,12 +1,26 @@
-""" Contains all the data for the game
-"""
-
-from random import choices , randint
 from time import time
+from random import choices
+import msvcrt
 
-### CONSTANTS ###
 
-## Dicts + Lists:
+### VARIABLES ###
+
+
+PrintSeparater = "print ('-----------------------------------------------')"
+EnvCleanupFactor = 50
+EnvInconsistency = 1.01
+EnvClusterSize = 5
+CollectDelay = 60
+MoveDelay = 5
+Red = '\033[31m'
+Green = '\033[32m'
+Blue = '\033[34m'
+White = '\033[37m'
+Black = '\033[30m'
+
+Environments = ["Woodlands" , "Plains" , "Grasslands" , "Waterlands" , "Rockylands"]
+BiomeWeights = [60 , 20 , 5 , 1]
+
 Commands = {
     "help" : "View the command list",
     "exit" : "Exit the program",
@@ -24,87 +38,11 @@ KeyBindings = {
     b"q" : ("Player .describe_surroundings ()" , "Give detailed location information"),
     b"e" : ("Player .look ()" , "Reveal the square ahead"),
     }
-Environments = ["Woodlands" , "Plains" , "Grasslands" , "Waterlands" , "Rockylands"]
-BiomeWeights = [60 , 20 , 5 , 1]
 
-## Values:
-PrintSeparater = "print ('-----------------------------------------------')"
-EnvCleanupFactor = 50
-EnvInconsistency = 1.01
-EnvClusterSize = 5
-CollectDelay = 60
-MoveDelay = 5
-
-
-### DEFINE CLASSES ###
-
-class Text:
-    Red = '\033[31m'
-    Green = '\033[32m'
-    Blue = '\033[34m'
-    White = '\033[37m'
-    Black = '\033[30m'
-
-class Environment:
-    def __init__ (self , PrimaryBiome , SecondaryBiome , TertiaryBiome , AbandonedStructure):
-        self .PriB = PrimaryBiome
-        self .SecB = SecondaryBiome
-        self .TerB = TertiaryBiome
-        self .Structure = AbandonedStructure
-    def choose (Surroundings):
-        Surroundings = [Square for Square in Surroundings if Square] 
-        EnvProbs = {Env : EnvInconsistency for Env in Environments}
-        for Square in Surroundings:
-            EnvProbs [Square] = EnvProbs [Square] * EnvClusterSize
-        EnvWeights = [Prob for Prob in list (EnvProbs.values ())]
-        EnvChoice = choices (Environments , weights=EnvWeights) [0]
-        return EnvChoice
-
-class Biome:
-    def __init__ (self , Name , Preposition , Ground , PrimaryResource , SecondaryResource , TertiaryResource , PrimaryMob , SecondaryMob , TertiaryMob , Animal , PrimaryLoot , SecondaryLoot):
-        self .Name = Name
-        self .Preposition = Preposition
-        self .GroundR = Ground
-        self .PriR = PrimaryResource
-        self .SecR = SecondaryResource
-        self .TerR = TertiaryResource
-        self .PriM = PrimaryMob
-        self .SecM = SecondaryMob
-        self .TerM = TertiaryMob
-        self .Animal = Animal
-        self .PriL = PrimaryLoot
-        self .SecL = SecondaryLoot
-    def choose (Env):
-        EnvClass = eval(Env)
-        BiomeChoice = choices ([EnvClass.PriB , EnvClass.SecB , EnvClass.TerB , EnvClass.Structure] , weights=BiomeWeights) [0]
-        return BiomeChoice
-
-class Animal:
-    pass
-
-class Item:
-    pass
-
-class Resource:
-    pass
-
-class Mob:
-    pass
-
-class Weapon:
-    def __init__ (self , Recipe , WeaponType , Damage , Speed , Accuracy , Durability):
-        self .Recipe = Recipe
-        self .Type = WeaponType
-        self .Damage = Damage
-        self .Speed = Speed
-        self .Accuracy = Accuracy
-
-class Armor:
-    def __init__ (self , Recipe , Protection , Durability):
-        self .Recipe = Recipe
-        self .Protection = Protection
-        self .Durability = Durability
-
+    
+### CLASS DEFINITIONS ###   
+    
+    
 class Character:
     def __init__(self , Name , Score , Map , BiomeMap , CollectTimeMap ,  Health , Energy , Inventory , HandItem , Armor , Location , Orientation , LastMoveTime):
         self .Name = Name
@@ -120,6 +58,7 @@ class Character:
         self .Orientation = Orientation
         self .LastMoveTime = LastMoveTime
         
+
     def look (self):
         try:
             if self .Orientation  == "North":
@@ -137,11 +76,13 @@ class Character:
             print ("You look %s and see %s %s." % (self.Orientation , Article , BiomeAhead))            
         except IndexError:
             print ("You see the edge of the world.")
+
         
     def describe_spawnpoint (self):
         CurrentBiome = self .BiomeMap [26][26] .Name
         AltPreposition = eval (CurrentBiome) .Preposition [:2] + eval (CurrentBiome) .Preposition [4:]
         print ("You spawn %s %s. You are facing North." % (AltPreposition , CurrentBiome))
+
 
     def describe_surroundings (self):
         CurrentBiome = self .BiomeMap [self.Location[0]-1][self.Location[1]-1] .Name
@@ -150,15 +91,18 @@ class Character:
         print ("\tCoordinates: " + str(self.Location))
         print ("\tEnvironment: " + CurrentEnv)
         print ("\tBiome: " + CurrentBiome)
+
     
     def set_name (self, NewName):
         self .Name = NewName
 
     def switch_hand_item (self, NewItem):
         self .HandItem = NewItem
+
         
     def switch_armor (self, NewArmor):
         self .Armor = NewArmor
+
     
     def move (self):
         if time () - self .LastMoveTime < MoveDelay: return
@@ -183,19 +127,82 @@ class Character:
             print ("You move %s %s %s." % (self.Orientation , eval(CurrentBiome).Preposition , CurrentBiome))            
         except IndexError:
             print ("You have reached the edge of the world.")
+
+
     def turn (self , NewOrientation):
         self .Orientation = NewOrientation
         print ("You turn to the %s." % NewOrientation)
 
+
     def collect (self):
         print ("collect")
+
+
     def attack (self):
         print ("attack")
 
-### CREATE INSTANCES ###
+        
+class Environment:
+    def __init__ (self , PrimaryBiome , SecondaryBiome , TertiaryBiome , AbandonedStructure):
+        self .PriB = PrimaryBiome
+        self .SecB = SecondaryBiome
+        self .TerB = TertiaryBiome
+        self .Structure = AbandonedStructure
 
-## Player:
-Player = Character ("Player 1" , 0 , [] , [] , [[time() - CollectDelay for Square in Row] for Row in Map] , 100 , 100 , {} , None , None , [26,26] , "North" , time())
+
+    def choose (Surroundings):
+        Surroundings = [Square for Square in Surroundings if Square] 
+        EnvProbs = {Env : EnvInconsistency for Env in Environments}
+        for Square in Surroundings:
+            EnvProbs [Square] = EnvProbs [Square] * EnvClusterSize
+        EnvWeights = [Prob for Prob in list (EnvProbs.values ())]
+        EnvChoice = choices (Environments , weights=EnvWeights) [0]
+        return EnvChoice
+
+        
+class Biome:
+    def __init__ (self , Name , Preposition , Ground , PrimaryResource , SecondaryResource , TertiaryResource , PrimaryMob , SecondaryMob , TertiaryMob , Animal , PrimaryLoot , SecondaryLoot):
+        self .Name = Name
+        self .Preposition = Preposition
+        self .GroundR = Ground
+        self .PriR = PrimaryResource
+        self .SecR = SecondaryResource
+        self .TerR = TertiaryResource
+        self .PriM = PrimaryMob
+        self .SecM = SecondaryMob
+        self .TerM = TertiaryMob
+        self .Animal = Animal
+        self .PriL = PrimaryLoot
+        self .SecL = SecondaryLoot
+
+
+    def choose (Env):
+        EnvClass = eval(Env)
+        BiomeChoice = choices ([EnvClass.PriB , EnvClass.SecB , EnvClass.TerB , EnvClass.Structure] , weights=BiomeWeights) [0]
+        return BiomeChoice
+
+        
+class Animal:
+    pass
+
+    
+class Item:
+    pass
+
+
+class Resource:
+    pass
+    
+
+class Mob:
+    pass
+
+
+#### INSTANCES ###
+
+
+## Player:    
+Player = Character ("Player 1" , 0 , [] , [] , [[time() - CollectDelay for Square in range (50)] for Row in range (50)] , 100 , 100 , {} , None , None , [26,26] , "North" , time())
 
 ## Resources:
 Bark = Resource ()
--- NewScript.py
+++ NewScript.py
@@ -1,32 +1,77 @@
 from Database import *
-from Game import *
-from random import randint , choice
-####import Image
+from random import choice
 
-### FUNCTIONS ###
 
-def startup ():
-    print ("NewScript Version 3")
-    print ("Type 'help' for the command list.")
-    while True:
-        command_input ()
+### GAME FUNCTIONS ###
 
-def command_input ():
-    eval (PrintSeparater)
-    Command = (input ("| ")) .lower ()
-    eval (PrintSeparater)
-    if Command in Commands : eval (Command + "()")
-    else : print ("Invalid Command")
 
 def create_player ():
     DesiredName = input ("Enter your name:\n| ")
     eval (PrintSeparater)
     Player .set_name (DesiredName)
 
-### COMMANDS ###
+    
+def load_map ():
+    print ("Loading Map")
+
+    ## Preload Map:
+    print ("\tCreating Seed")
+    Map = [[None 
+            if ColIndex in [0,51]
+            or RowIndex in [0,51]
+            else choices (("Woodlands" , "Plains" , "Grasslands" , "Waterlands" , "Rockylands")) [0]
+        for ColIndex in range (52)]
+        for RowIndex in range (52)]
+    
+    ## Choose Environments:
+    print ("\tGenerating Chunks")
+    for Rep in range (EnvCleanupFactor):
+        Map = [[None
+                if ColIndex in [0,51]
+                or RowIndex in [0,51]
+                else Environment .choose ([Map[RowIndex-1][ColIndex-1] , Map[RowIndex-1][ColIndex] , Map[RowIndex-1][ColIndex+1] , Map[RowIndex][ColIndex-1] , Map[RowIndex][ColIndex+1] , Map[RowIndex+1][ColIndex-1] , Map[RowIndex+1][ColIndex] , Map[RowIndex+1][ColIndex+1]])
+            for ColIndex in range(len(Map[RowIndex]))]
+            for RowIndex in range(len(Map))]
+    Map = [[Env for Env in Row[1:-1]] for Row in Map[1:-1]]
+
+    ## Choose Biomes:
+    print ("\tGenerating Terrain")
+    BiomeMap = [[Biome .choose (Env)
+        for Env in Row]
+        for Row in Map]
+    
+    Player .Map = Map
+    Player .BiomeMap = BiomeMap
+
+    return Map , BiomeMap
+
+
+def run_game ():
+    ## Wait for keypress:
+    while True:
+        while not msvcrt .kbhit ():
+            pass
+            #### Update game
+        KeyInput = msvcrt .getch ()
+        
+        ## Respond to keypress:
+        if KeyInput in KeyBindings:
+            eval (KeyBindings [KeyInput][0])
+
+
+def update_game ():
+    #### Update health and energy
+    #### Regenerate resources in exhausted squares
+    #### Spawn stuff
+    pass
+
+
+### COMMAND FUNCTIONS ###
+
 
 def help ():
     [print (Command+ ": " + Commands[Command]) for Command in Commands]
+
 
 def start ():
     create_player ()
@@ -35,16 +80,41 @@ def start ():
     Player .describe_spawnpoint ()
     run_game ()
 
+
+def startdev ():
+    #### same as start
+    #### but somehow enable input
+    #### for developer testing options
+    #### developer password:
+    #### Hashing check
+
+
 def controls ():
     for Key in KeyBindings:
         if Key == b" ": Letter = "SPACE"
         else: Letter = Key .decode ("utf-8")
         print (Letter + ": " + KeyBindings [Key][1])
         
-    [print (Command+ ": " + Commands[Command]) for Command in Commands]
-    
-    #### Spawn function (set location and stats)
-    #### run_game () (will endlessly update game while waiting for input)
+    [print (Command+ ": " + Commands[Command]) for Command in Commands]        
 
+
+### MANAGER FUNCTIONS ###
+
+
+def command_input ():
+    eval (PrintSeparater)
+    Command = (input ("| ")) .lower ()
+    eval (PrintSeparater)
+    if Command in Commands : eval (Command + "()")
+    else : print ("Invalid Command")
+
+    
+def startup ():
+    print ("NewScript Version 3")
+    print ("Type 'help' for the command list.")
+    while True:
+        command_input ()    
+
+        
 if __name__ == "__main__" : startup ()
 else : pass #### Check for CorPC sample name.
