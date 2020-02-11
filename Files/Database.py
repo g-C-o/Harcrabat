6a54236f938cb7eb57bf15b4f3a929bd58b636dd
Cameron White <cameronw@davidsonacademy.unr.edu>
2020-02-11 10:15:37 -0800

    Change recipe items to objects

--- Files/Database.py
+++ Files/Database.py
@@ -14,8 +14,8 @@ from colorama import Fore, Back, init, Style
 ENV_CLEANUP_FACTOR = 50
 ENV_INCONSISTENCY = 1.01
 ENV_CLUSTER_SIZE = 5
-COLLECT_DELAY = 0 #### 60
-MOVE_DELAY = 0 #### 5
+COLLECT_DELAY = 60
+MOVE_DELAY = 5
 HARVEST_SIZE = 4
 UNCOMMON_RESOURCE_THRESHOLD = 75
 RARE_RESOURCE_THRESHOLD = 16
@@ -25,7 +25,7 @@ PRINT_SEPARATER = "print('-----------------------------------------------')"
 ### LISTS ###
 
 
-ITEMS = ["Soil", "Wood", "Vine", "Fruit", "Rock", "Water", "Sand", "Cacti", "Iron", "Bone", "Diamond", "Stone", "Gold", "Emerald", "Quartz", "Explosive", "HarvesterArmor", "Protector", "Strawman", "Coffin", "Ladder", "MobRepellent", "BottledWave", "BottledWind", "Glider", "Binoculars", "WoodenBall", "StoneBall", "IronBall", "DiamondBall", "WoodenArrow", "StoneArrow", "IronArrow", "DiamondArrow", "SoilClump", "SandClump", "CactusClump", "WoodSpike", "StoneSpike", "IronSpike", "DiamondSpike", "CannonBall", "Rocket", "Missile", "Bow", "Crossbow", "Slingshot", "Cannon", "RocketLauncher", "MissileLauncher", "BoneBlade", "BoneStriker", "WoodenSword", "StoneSword", "IronSword", "DiamondSword", "WoodenAxe", "StoneAxe", "IronAxe", "DiamondAxe", "WoodenSpear", "StoneSpear", "IronSpear", "DiamondSpear", "WoodenBoomerang", "StoneBoomerang", "IronBoomerang", "DiamondBoomerang", "Bomb", "Grenade", "Dynamite"]
+ITEMS = ["Soil", "Wood", "Vine", "Fruit", "Rock", "Water","Sand", "Cacti", "Iron", "Bone", "Diamond", "Stone", "Gold", "Emerald", "Quartz", "Explosive", "HarvesterArmor", "Protector", "Strawman", "Coffin", "Ladder", "MobRepellent", "BottledWave", "BottledWind", "Glider", "Binoculars", "WoodenBall", "StoneBall", "IronBall", "DiamondBall", "WoodenArrow", "StoneArrow", "IronArrow", "DiamondArrow", "SoilClump", "SandClump", "CactusClump", "WoodSpike", "StoneSpike", "IronSpike", "DiamondSpike", "CannonBall", "Rocket", "Missile", "Bow", "Crossbow", "Slingshot", "Cannon", "RocketLauncher", "MissileLauncher", "BoneBlade", "BoneStriker", "WoodenSword", "StoneSword", "IronSword", "DiamondSword", "WoodenAxe", "StoneAxe", "IronAxe", "DiamondAxe", "WoodenSpear", "StoneSpear", "IronSpear", "DiamondSpear", "WoodenBoomerang", "StoneBoomerang", "IronBoomerang", "DiamondBoomerang", "Bomb", "Grenade", "Dynamite"]
 ENVIRONMENTS = ["Woodlands", "Plains", "Grasslands", "Waterlands", "Rockylands"] #### Change to object in main
 BIOME_WEIGHTS = [60, 20, 5, 1]
 RESOURCE_WEIGHTS = [60, 35, 5]
